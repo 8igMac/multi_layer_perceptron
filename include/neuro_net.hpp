dc268f38d4b9d14e7d93f920fb8b3477f9401b3e
8igMac <michaelshih123@gmail.com>
2018-10-19 15:29:03 +0800

    class NeuroNet structure

---
+++ include/neuro_net.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include <vector>
+
+template <typename DATA_TYPE>
+class NeuroNet
+{
+  public:
+    // constructor
+    NeuroNet()
+    {
+
+    }
+
+    // train
+    void train(std::vector<DATA_TYPE> train_data)
+    {
+
+    }
+
+    // validate
+    double validate(std::vector<DATA_TYPE> vali_data)
+    {
+
+    }
+
+    // stop criterion
+
+  private:
+    // data member
+};
